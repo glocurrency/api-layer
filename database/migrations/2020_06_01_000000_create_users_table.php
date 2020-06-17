808e18896a8bab888e38c4a4b4161124329cdff5
Ivan St <brokeyourbike@gmail.com>
2020-06-17 17:06:29 +0300

    feat: add user migration

---
+++ database/migrations/2020_06_01_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('codename')->unique()->index();
+            $table->string('description')->nullable();
+            $table->string('url')->nullable();
+            $table->boolean('enabled')->default(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
