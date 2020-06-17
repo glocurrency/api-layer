b02d3c18ec3b4197a2a67d03d13381a12c448227
Ivan St <brokeyourbike@gmail.com>
2020-06-17 18:27:28 +0300

    fix: add proper route registar

---
+++ src/RouteRegistrar.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Glocurrency\ApiLayer;
+
+use Illuminate\Contracts\Routing\Registrar as Router;
+
+class RouteRegistrar
+{
+    /**
+     * The router implementation.
+     *
+     * @var \Illuminate\Contracts\Routing\Registrar
+     */
+    protected $router;
+
+    /**
+     * Create a new route registrar instance.
+     *
+     * @param  \Illuminate\Contracts\Routing\Registrar  $router
+     * @return void
+     */
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * Register routes for transient tokens, clients, and personal access tokens.
+     *
+     * @return void
+     */
+    public function all()
+    {
+        $this->forPing();
+    }
+
+    /**
+     * Register the routes needed for authorization.
+     *
+     * @return void
+     */
+    public function forPing()
+    {
+        $this->router->group(['middleware' => ['api']], function ($router) {
+            $router->get('/ping', [
+                'uses' => 'PingController@ping',
+            ]);
+        });
+    }
+}
