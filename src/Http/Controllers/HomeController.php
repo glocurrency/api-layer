e94d779d396e8cdc24b5cf2c539a8da679419df1
Ivan St <brokeyourbike@gmail.com>
2020-07-14 08:03:40 +0300

    feat: add home controller

---
+++ src/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Glocurrency\ApiLayer\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class HomeController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function index(Request $request)
+    {
+        return view('welcome');
+    }
+}
